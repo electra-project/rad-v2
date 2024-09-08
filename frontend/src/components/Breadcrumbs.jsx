cb9fb06cef91fa7aac94062f07ed3083003f48d3
SW33TSTUFF <simaakniyaz@gmail.com>
2024-09-08 14:45:36 +0530

    Fixed Breadcrumb component

--- frontend/src/components/Breadcrumbs.jsx
+++ frontend/src/components/Breadcrumbs.jsx
@@ -1,24 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { HomeIcon, ChevronRightIcon } from "lucide-react";
 
 const Breadcrumbs = ({ categoryName }) => {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+
   return (
     <nav className="flex items-center text-sm font-medium text-gray-400 mb-4">
       <Link to="/" className="flex items-center hover:text-white">
         <HomeIcon className="w-4 h-4 mr-2" />
         Home
       </Link>
-      <ChevronRightIcon className="w-4 h-4 mx-2" />
-      <Link to="/store/category" className="hover:text-white">
-        Store
-      </Link>
-      <ChevronRightIcon className="w-4 h-4 mx-2" />
-      <Link to="/store/category" className="hover:text-white">
-        Category
-      </Link>
-      <ChevronRightIcon className="w-4 h-4 mx-2" />
-      <span className="text-white">{categoryName}</span>
+      {pathnames.map((name, index) => {
+        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+        const isLast = index === pathnames.length - 1;
+
+        return (
+          <React.Fragment key={name}>
+            <ChevronRightIcon className="w-4 h-4 mx-2" />
+            {isLast ? (
+              <span className="text-white capitalize">
+                {categoryName || name}
+              </span>
+            ) : (
+              <Link to={routeTo} className="hover:text-white capitalize">
+                {name}
+              </Link>
+            )}
+          </React.Fragment>
+        );
+      })}
     </nav>
   );
 };
